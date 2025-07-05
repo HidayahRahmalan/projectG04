b4c7424b41332bd5f0722786b62d442d1b5a4ec2
lyanayana <lyanaazmi03@gmail.com>
2025-07-05 13:36:49 +0800

    Update connection.php

--- G04_02_FixMasterMaintenance/connection.php
+++ G04_02_FixMasterMaintenance/connection.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "localhost";
 $username = "group2";
-$password = "@Lyana2003";
-$dbname = "p25_MaintenanceDB";
+$password = "group2";
+$dbname = "p25_maintenancedb";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
