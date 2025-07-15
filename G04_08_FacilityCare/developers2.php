4b4664e10725bba22f18ed8ab42f780951c0dfab
kmadaniel <imadaniel2004@gmail.com>
2025-07-15 20:43:57 +0800

    Update developers2.php

--- G04_08_FacilityCare/developers2.php
+++ G04_08_FacilityCare/developers2.php
@@ -43,7 +43,7 @@ $user = $stmt->fetch();
             left: 0;
             right: 0;
             bottom: 0;
-            background: url('images/G4_08_FacilityCare.jpg') center/contain no-repeat;
+            background: url('images/your_image.jpg') center/contain no-repeat;
             opacity: 0.03;
             z-index: 0;
         }
