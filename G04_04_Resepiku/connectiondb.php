db1c7f4e8a1476edc900e93f1ff926fbbc3510c0
SyahmieAmirullah <120726922+SyahmieAmirullah@users.noreply.github.com>
2025-07-16 15:13:35 +0800

    Update connectiondb.php

--- G04_04_Resepiku/connectiondb.php
+++ G04_04_Resepiku/connectiondb.php
@@ -1,14 +1,13 @@
 <?php
-$hostname = "localhost";
-$username = "resepiku";
-$password = "123456";
-$dbname = "p25_resepiku";
+ $host="localhost";
+ $user="resepiku";
+ $pass="123456"; 
+ $dbname="p25_resepiku";
 
-$conn = new mysqli($hostname, $username, $password, $dbname);
-
- // Check connection
+ $conn = new mysqli($host, $user, $pass, $dbname);
+// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
-}
-echo "Connected successfully lah weiii"; 
+}   
+
 ?>
