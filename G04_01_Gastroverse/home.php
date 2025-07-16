e491b39593207e9402a98f9afd915a80804b2664
Allim07 <168447787+Allim07@users.noreply.github.com>
2025-07-16 21:42:14 +0800

    Update home.php

--- G04_01_Gastroverse/home.php
+++ G04_01_Gastroverse/home.php
@@ -1,14 +1,8 @@
 <?php
 include('header.php');
 
-// Database connection
-$host = '127.0.0.1:3306';
-$dbname = 'gastroverse1';
-$username = 'root';
-$password = '';
-
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $pdo = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Database connection failed: " . $e->getMessage());
