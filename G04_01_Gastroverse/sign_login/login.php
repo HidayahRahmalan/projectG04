e16c4fc7b310cd30fa61395c2854528de204eb77
Allim07 <168447787+Allim07@users.noreply.github.com>
2025-07-16 21:15:23 +0800

    Update login.php

--- G04_01_Gastroverse/sign_login/login.php
+++ G04_01_Gastroverse/sign_login/login.php
@@ -26,27 +26,27 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
    
            if ($user['User_Role'] === 'chef') {
     $_SESSION['welcome'] = "Welcome " . htmlspecialchars($_SESSION['UserName']) . "!";
-    header("Location: /G04_01_Gastroverse/home.php");
+    header("Location: ../home.php");
     exit();
 } elseif ($user['User_Role'] === 'student') {
     $_SESSION['welcome'] = "Welcome " . htmlspecialchars($_SESSION['UserName']) . "!";
-    header("Location: /G04_01_Gastroverse/home.php");
+    header("Location: ../home.php");
     exit();
 } elseif ($user['User_Role'] === 'admin') {
     $_SESSION['welcome'] = "Welcome " . htmlspecialchars($_SESSION['UserName']) . "!";
-    header("Location: /G04_01_Gastroverse/admin.php");
+    header("Location: ../admin.php");
     exit();
 }
         } else {
             echo "<script>
             sessionStorage.setItem('message', 'Invalid password.');
-            window.location.href = '/G04_01_Gastroverse/landingpage.php';
+            window.location.href = '../landingpage.php';
             </script>";
         }
     } else {
         echo "<script>
             sessionStorage.setItem('message', 'Invalid email address.');
-            window.location.href = '/G04_01_Gastroverse/landingpage.php';
+            window.location.href = '../landingpage.php';
             </script>";
     }
 
