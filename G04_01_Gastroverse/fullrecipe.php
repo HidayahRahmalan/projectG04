fed691e2aa634d43de0de99d1fe94aa0532a6c39
Allim07 <168447787+Allim07@users.noreply.github.com>
2025-07-16 21:42:40 +0800

    Update fullrecipe.php

--- G04_01_Gastroverse/fullrecipe.php
+++ G04_01_Gastroverse/fullrecipe.php
@@ -2,13 +2,8 @@
 include('header.php');
 include('connect.php'); 
 
-$host = '127.0.0.1:3306';
-$dbname = 'gastroverse1';
-$username = 'root';
-$password = '';
-
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $pdo = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Database connection failed: " . $e->getMessage());
